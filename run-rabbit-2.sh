44675df123537b4aa4a7941ba601731af46bcc84
Tran Dung Chien <trandungchien1982@gmail.com>
2026-03-29 11:45:01 +0700

    Update 4

---
+++ run-rabbit-2.sh
@@ -0,0 +1,1 @@
+ansible-playbook -i inventory2.yml playbook2.yml
--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
+echo Update the setup-ips.yml
+curl -s https://raw.githubusercontent.com/trandungchien1982/provisioning/refs/heads/main/setup-ips.yml -o ./value_out.txt
 ansible-playbook -i inventory.yml playbook.yml
