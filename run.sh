bbbd93e4e6a5206ce21ba1e1d5210cb7d0c78aff
Tran Dung Chien <trandungchien1982@gmail.com>
2026-03-29 15:09:19 +0700

    Delete unused

--- run.sh
+++
@@ -1,3 +0,0 @@
-echo Update the setup-ips.yml
-curl -s https://raw.githubusercontent.com/trandungchien1982/provisioning/refs/heads/main/setup-ips.yml -o ./setup-ips.yml
-ansible-playbook -i inventory.yml playbook.yml
