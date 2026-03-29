f1bee484f990d7d41ae5a42b021a5b260976ab6f
Tran Dung Chien <trandungchien1982@gmail.com>
2026-03-29 14:52:27 +0700

    Update 9

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 echo Update the setup-ips.yml
-curl -s https://raw.githubusercontent.com/trandungchien1982/provisioning/refs/heads/main/setup-ips.yml -o ./value_out.txt
+curl -s https://raw.githubusercontent.com/trandungchien1982/provisioning/refs/heads/main/setup-ips.yml -o ./setup-ips.yml
 ansible-playbook -i inventory.yml playbook.yml
