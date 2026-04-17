d3726dba336e5d963a8ca900b9a7b8907c45ca79
tdc <tdc@ubuntu.laptop>
2026-04-17 17:35:20 +0700

    Apply 12.GrafanaLogs-Multipass

--- 02.start-grafana-loki.sh
+++ 02.start-grafana-loki.sh
@@ -7,10 +7,12 @@ APP_NAMING="grafana-loki"
 # Start Grafana+Loki with following information:
 echo "==> Start Grafana+Loki..."
 echo "===================================="
-multipass exec $VM_NAME -- mkdir -p /opt/loki/{config,data}
+multipass exec $VM_NAME -- sudo mkdir -p /opt/loki/{config,data}
+multipass exec $VM_NAME -- sudo chmod 777 /opt/loki
+multipass exec $VM_NAME -- sudo chmod 777 /opt/loki/data
 
-multipass transfer loki-config.yaml $VM_NAME:/opt/loki/config/loki-config.yaml
 multipass transfer docker-compose.yaml $VM_NAME:/opt/loki
+multipass transfer loki-config.yaml $VM_NAME:/opt/loki
 
 multipass transfer zz.02.start-$APP_NAMING.sh $VM_NAME:/home/ubuntu/
 multipass exec $VM_NAME -- chmod +x /home/ubuntu/zz.02.start-$APP_NAMING.sh
