b060b227ea1e8e433d5eadfb830e6770c99043de
tdc <tdc@ubuntu.laptop>
2026-05-01 22:43:10 +0700

    Apply 15.ElasticSearch+Kibana - No Authentication ES + Kibana

--- 02.start-els-kibana.sh
+++ 02.start-els-kibana.sh
@@ -8,19 +8,11 @@ APP_NAMING="zz.02.start-els-kibana"
 echo "==> Start ElasticSearch+Kibana ..."
 echo "===================================="
 multipass transfer $APP_NAMING.sh $VM_NAME:/home/ubuntu/
-multipass transfer -r ./config $VM_NAME:/home/ubuntu/
 multipass exec $VM_NAME -- chmod +x /home/ubuntu/$APP_NAMING.sh
 
 # Copy all config files
 multipass transfer *.yaml $VM_NAME:/home/ubuntu/
 multipass transfer *.sh $VM_NAME:/home/ubuntu/
-
-multipass exec $VM_NAME -- chmod +x /home/ubuntu/bootstrap.sh
-multipass exec $VM_NAME -- chmod +x /home/ubuntu/kibana-entrypoint.sh
-
-multipass exec $VM_NAME -- mkdir -p /home/ubuntu/data/elasticsearch/data || true
-multipass exec $VM_NAME -- mkdir -p /home/ubuntu/data/elasticsearch/backups || true
-multipass exec $VM_NAME -- mkdir -p /home/ubuntu/data/kibana/log || true
 
 multipass exec $VM_NAME -- /home/ubuntu/$APP_NAMING.sh
 echo "===================================="
