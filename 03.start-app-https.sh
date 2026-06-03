aa3ea1953173c26bc4cc8752accab44456a4c160
tdc <tdc@ubuntu.laptop>
2026-06-03 16:00:48 +0700

    Apply 25.ReverseProxy.nginx

--- 03.start-app-https.sh
+++ 03.start-app-https.sh
@@ -8,9 +8,9 @@ echo "==> Start App ..."
 echo "===================================="
 # Copy all config files (docker-compose, etc ...)
 multipass transfer *.* $VM_NAME:/home/ubuntu/
-#multipass exec $VM_NAME -- sudo mkdir -p /home/ubuntu/nginx/https-conf.d
-multipass transfer ./nginx/https-conf.d/* $VM_NAME:/home/ubuntu/nginx
-multipass transfer ./certs/* $VM_NAME:/home/ubuntu/nginx
+multipass exec $VM_NAME -- mkdir -p /home/ubuntu/certs /home/ubuntu/nginx/https-conf.d
+multipass transfer ./nginx/https-conf.d/* $VM_NAME:/home/ubuntu/nginx/https-conf.d
+multipass transfer ./certs/* $VM_NAME:/home/ubuntu/certs
 
 # To be executed :)
 multipass exec $VM_NAME -- chmod +x /home/ubuntu/$APP_NAMING.sh
